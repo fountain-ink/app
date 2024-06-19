8842482d5da4e9d123d80515878dc17d40de592f
kualta <contact@kualta.dev>
2024-06-19 23:16:22 +0300

    add tiptap

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,10 +1,10 @@
+import Tiptap from "~/components/TIptap";
+
 export default function HomePage() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center bg-[#d0dff7] text-[#432a21]">
 			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-				<h1 className="text-2xl font-extrabold tracking-tight sm:text-[5rem]">
-					Fountain editor
-				</h1>
+         <Tiptap />
 			</div>
 		</main>
 	);
