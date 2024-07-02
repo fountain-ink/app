4ced18292794af87557724457d510d50d31171f5
kualta <contact@kualta.dev>
2024-07-02 11:13:04 +0300

    update article min height

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center text-foreground bg-background">
 			<div className="container flex flex-col items-center justify-center w-full max-w-lg md:max-w-xl lg:max-w-2xl lg:max-w-4xl ">
-				<div className="w-full">
+				<div className="w-full min-h-screen py-4">
 					<ThemeSwitcher />
 					<EditorDate />
 					<EditorCollaborators />
