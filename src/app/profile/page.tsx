ef4f058bd86c158b289dd2770e9fd4b920ac3f74
kualta <contact@kualta.dev>
2024-08-06 23:07:15 +0300

    add lens profile hook

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { useProfile } from "@lens-protocol/react-web";
 import { ConnectKitButton } from "connectkit";
 
-export default function HomePage() {
+export default function ProfilePage() {
+	const { data } = useProfile({
+		forHandle: "lens/stani",
+		suspense: true,
+	});
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center bg-[#d0dff7] text-[#432a21]">
 			<div className="container flex flex-col items-center justify-center w-full max-w-lg lg:max-w-xl ">
