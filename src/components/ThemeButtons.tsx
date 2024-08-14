56cb7fccbb1bfed45b0b8761277f6728a247c25e
kualta <contact@kualta.dev>
2024-08-14 11:20:00 +0300

    update theme buttons

--- src/components/ThemeButtons.tsx
+++ src/components/ThemeButtons.tsx
@@ -9,20 +9,25 @@ export const ThemeButtons = () => {
 
 	const themeButtons = Object.values(themeNames).map((theme) => {
 		const accent = themes[theme]["--accent"];
+		const foreground = themes[theme]["--foreground"];
+		const background = themes[theme]["--background"];
+		const themeName = theme;
+		const placeholderText =
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
 		return (
 			<Button
 				variant="outline"
-				className="flex gap-2 w-full h-24"
+				className="flex flex-col items-start overflow-visible text-left gap-2 w-full h-fit p-8"
+				style={{
+					backgroundColor: `hsl(${background})`,
+					color: `hsl(${foreground})`,
+				}}
 				key={theme}
 				onClick={() => setTheme(theme)}
 			>
-				<div
-					className="w-6 h-6 rounded-full"
-					style={{ backgroundColor: `hsl(${accent})` }}
-				/>
-
-				{theme}
+				<h1 className="font-bold text-5xl font-martina">{themeName}</h1>
+				<p className="text-md break-all whitespace-normal">{placeholderText}</p>
 			</Button>
 		);
 	});
