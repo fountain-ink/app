a98ff63eed0c4dba8ebafc726f3d912f29a03a32
kualta <contact@kualta.dev>
2024-08-20 11:15:51 +0000

    fix blur shown over drawer

--- src/components/Blur.tsx
+++ src/components/Blur.tsx
@@ -1,8 +1,8 @@
 export const Blur = () => {
 	return (
 		<>
-			<div className="z-[100] gradient-blur-top h-[35%] w-full fixed top-0 pointer-events-none " />
-			<div className="z-[100] gradient-blur-bot h-[35%] w-full fixed bottom-0 pointer-events-none " />
+			<div className="z-[35] gradient-blur-top h-[35%] w-full fixed top-0 pointer-events-none " />
+			<div className="z-[35] gradient-blur-bot h-[35%] w-full fixed bottom-0 pointer-events-none " />
 		</>
 	);
 };
