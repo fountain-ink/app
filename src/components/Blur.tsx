68d2c6ec30e671ca330737a60258e38f3cde670b
kualta <contact@kualta.dev>
2024-08-20 12:55:23 +0000

    fix blur jitter on edges

--- src/components/Blur.tsx
+++ src/components/Blur.tsx
@@ -1,8 +1,22 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 export const Blur = () => {
+	const pathname = usePathname();
+	console.log(pathname);
+
+	if (pathname !== "/" && !pathname.startsWith("/p")) {
+		return null;
+	}
+
 	return (
 		<>
-			<div className="z-[35] gradient-blur-top h-[35%] w-full fixed top-0 pointer-events-none " />
-			<div className="z-[35] gradient-blur-bot h-[35%] w-full fixed bottom-0 pointer-events-none " />
+			<div className="z-[36] h-[10%] w-full fixed top-0 bg-gradient-to-b from-background to-transparent" />
+			<div className="z-[35] gradient-blur-top h-[35%] w-full fixed top-0" />
+
+			<div className="z-[35] gradient-blur-bot h-[35%] w-full fixed bottom-0" />
+			<div className="z-[36] h-[10%] w-full fixed bottom-0 bg-gradient-to-t from-background to-transparent" />
 		</>
 	);
 };
--- src/components/user/UserSocials.tsx
+++ src/components/user/UserSocials.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Profile } from "@lens-protocol/react-web";
+import type { Profile } from "@lens-protocol/react-web";
 
 export const UserSocials = ({ profile }: { profile?: Profile }) => {
 	return <div />;
