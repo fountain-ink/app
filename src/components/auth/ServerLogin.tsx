ad03613f2ca082c3d50a3b8359283a7f02c92311
kualta <contact@kualta.dev>
2024-08-22 13:52:02 +0300

    set db auth cookie server-side

--- src/components/auth/ServerLogin.tsx
+++ src/components/auth/ServerLogin.tsx
@@ -1,11 +1,22 @@
 "use server";
 
+import { env } from "@/env";
 import { LensClient, production } from "@lens-protocol/client";
-import { toast } from "sonner";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
 
 const client = new LensClient({
 	environment: production,
 });
+
+interface LensJwtToken {
+	evmAddress: string;
+	id: string;
+	role: string;
+	authorizationId: string;
+	iat: number;
+	exp: number;
+}
 
 export async function serverLogin(refreshToken: string) {
 	"use server";
@@ -13,12 +24,26 @@ export async function serverLogin(refreshToken: string) {
 	// Check lens JWT token
 	await client.authentication.authenticateWith({ refreshToken });
 	const isAuthenticated = await client.authentication.isAuthenticated();
+	const lensJwtData = jwt.decode(refreshToken) as LensJwtToken;
 
-	if (!isAuthenticated) {
+	if (!isAuthenticated || !lensJwtData) {
 		throw new Error("Invalid identity token");
 	}
 
-	const profileId = await client.authentication.getProfileId();
+	const profileId = lensJwtData.id;
+	const address = lensJwtData.evmAddress;
+
+	const supabaseJwt = jwt.sign(
+		{ profileId, address, role: "user" },
+		env.SUPABASE_JWT_SECRET,
+	);
 
 	console.log(`Logged in as ${profileId}`);
+
+	cookies().set("db-auth", supabaseJwt, {
+		path: "/",
+		secure: true,
+		httpOnly: true,
+		sameSite: "lax",
+	});
 }
