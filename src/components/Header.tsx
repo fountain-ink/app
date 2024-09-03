dd56671928c15a9871abf91435a1540ff1fd8376
kualta <contact@kualta.dev>
2024-09-03 05:46:31 +0000

    add prod check in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,14 @@ import { WriteMenu } from "./article/WriteMenu";
 import { FountainLogo } from "./Icons";
 import { ThemeSidebar } from "./ThemeEditor";
 import { UserMenu } from "./user/UserMenu";
+import { getBaseUrl } from "@/lib/getBaseUrl";
+import { isProdEnvironment } from "@/lib/envCheck";
 
 export const Header = () => {
 	const pathname = usePathname();
 
 	// FIXME: Temporary before release
-	if (pathname === "/") {
+	if (isProdEnvironment) {
 		return (
 			<div className="fixed w-full p-2 z-[300] flex justify-between items-center pointer-events-none">
 				<Link
---
+++ src/lib/envCheck.ts
@@ -0,0 +1,3 @@
+export const isDevEnvironment = process.env.NODE_ENV === "development";
+export const isProdEnvironment = process.env.NODE_ENV === "production";
+export const isTestEnvironment = process.env.NODE_ENV === "test";
