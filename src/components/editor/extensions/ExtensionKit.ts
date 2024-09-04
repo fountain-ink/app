fa73fe52da97e353bf680c680730e60014e44f0c
kualta <contact@kualta.dev>
2024-09-04 15:10:26 +0400

    update global rounding

--- src/components/editor/extensions/ExtensionKit.ts
+++ src/components/editor/extensions/ExtensionKit.ts
@@ -1,11 +1,11 @@
 "use client";
 
-import { HocuspocusProvider } from "@hocuspocus/provider";
-
-
+import type { HocuspocusProvider } from "@hocuspocus/provider";
 import CharacterCount from "@tiptap/extension-character-count";
-import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import Color from "@tiptap/extension-color";
 import Document from "@tiptap/extension-document";
+import Dropcursor from "@tiptap/extension-dropcursor";
+import Focus from "@tiptap/extension-focus";
 import FontFamily from "@tiptap/extension-font-family";
 import Heading from "@tiptap/extension-heading";
 import Highlight from "@tiptap/extension-highlight";
@@ -13,28 +13,24 @@ import HorizontalRule from "@tiptap/extension-horizontal-rule";
 import ImageBlock from "@tiptap/extension-image";
 import Link from "@tiptap/extension-link";
 import Placeholder from "@tiptap/extension-placeholder";
+import Subscript from "@tiptap/extension-subscript";
+import Superscript from "@tiptap/extension-superscript";
 import Table from "@tiptap/extension-table";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
-import StarterKit from "@tiptap/starter-kit";
-import Subscript from "@tiptap/extension-subscript";
-import Superscript from "@tiptap/extension-superscript";
+import TextAlign from "@tiptap/extension-text-align";
 import TextStyle from "@tiptap/extension-text-style";
 import Typography from "@tiptap/extension-typography";
-import Paragraph from '@tiptap/extension-paragraph'
 import Underline from "@tiptap/extension-underline";
-import TextAlign from "@tiptap/extension-text-align";
-import Focus from "@tiptap/extension-focus";
-import Color from "@tiptap/extension-color";
-import FontSize from "./FontSize";
-import Figcaption from "./Figcaption";
+import StarterKit from "@tiptap/starter-kit";
 import BlockquoteFigure from "./BlockquoteFigure";
-import Dropcursor from "@tiptap/extension-dropcursor";
-import { TrailingNode } from "./TrailingNode";
+import Figcaption from "./Figcaption";
+import FontSize from "./FontSize";
 import { Selection } from "./Selection";
+import { TrailingNode } from "./TrailingNode";
 // import { CodeBlockLowlight } from '@tiptap/extension-code-block-lowlight'
 // import { ImageUpload } from './ImageUpload'
 // import { TableOfContentsNode } from './TableOfContentsNode'
