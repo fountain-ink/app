fa638b76237d6cab6681c0bfcabd5f2fb11e012f
kualta <contact@kualta.dev>
2024-09-16 17:24:55 +0400

    sanitize html

--- src/app/p/[post]/page.tsx
+++ src/app/p/[post]/page.tsx
@@ -1,31 +1,36 @@
 import { getAuthorizedClients } from "@/lib/get-auth-clients";
 
+import { sanitize } from "isomorphic-dompurify";
+
 const post = async ({ params }: { params: { post: string } }) => {
   const { lens } = await getAuthorizedClients();
 
   const id = params.post;
-  const lensPost = await lens.publication.fetch({
+  const post = await lens.publication.fetch({
     forId: id,
   });
 
-  if (!lensPost) throw new Error("(╥_╥) Post not found");
-
-  console.log(lensPost);
+  if (!post) throw new Error("(╥_╥) Post not found");
 
   if (
-    lensPost.__typename === "Post" &&
-    lensPost.metadata.__typename === "ArticleMetadataV3" &&
-    lensPost.metadata.appId === "fountain"
+    post.__typename === "Post" &&
+    post.metadata.__typename === "ArticleMetadataV3" &&
+    post.metadata.appId === "fountain"
   ) {
-    const _contentJson = lensPost?.metadata?.attributes?.find((attr: any) => attr.key === "contentJson")?.value;
-    const contentHtml = lensPost?.metadata?.attributes?.find((attr: any) => attr.key === "contentHtml")?.value;
+    const _contentJson = post?.metadata?.attributes?.find((attr: any) => attr.key === "contentJson")?.value;
+    const contentHtml = post?.metadata?.attributes?.find((attr: any) => attr.key === "contentHtml")?.value;
+
+    if (!contentHtml) throw new Error("(╥_╥) Post not found");
 
     return (
       <div className="container flex flex-col items-center justify-center w-full max-w-lg md:max-w-xl lg:max-w-2xl">
         <div className="w-full min-h-screen py-4 my-10">
           <div
             className="prose prose-sm sm:prose-base prose-img:prose-h1:font-martina prose-h1:my-8 prose-h1:text-center prose-h1:text-6xl lg:prose-lg focus:outline-none rounded-lg"
-            dangerouslySetInnerHTML={{ __html: contentHtml }}
+            // biome-ignore lint/security/noDangerouslySetInnerHtml: intended use
+            dangerouslySetInnerHTML={{ __html: sanitize(contentHtml) }}
+
+            
           />
         </div>
       </div>
