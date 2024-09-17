bd47ff9f3508a755cb97e66eb5e30c6c14067763
kualta <contact@kualta.dev>
2024-09-17 21:22:45 +0400

    improve post view

--- src/components/post/post-view.tsx
+++ src/components/post/post-view.tsx
@@ -1,7 +1,7 @@
 import type { ArticleMetadataV3, Post, ProfileId } from "@lens-protocol/react-web";
 import Link from "next/link";
 import Markdown from "../content/markdown";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "../ui/card";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
 import { UserAuthorView } from "../user/user-author-view";
 import { PostReactions } from "./post-reactions";
 
@@ -12,7 +12,6 @@ interface PostViewProps {
 
 export const PostView = ({ post, authorIds }: PostViewProps) => {
   const metadata = post.metadata as ArticleMetadataV3;
-  const title = metadata?.title || "Untitled";
   const content = metadata?.content || "";
 
   const formattedDate = new Date(post.createdAt).toLocaleDateString("en-US", {
@@ -24,11 +23,10 @@ export const PostView = ({ post, authorIds }: PostViewProps) => {
   return (
     <Link href={`/p/${post.id}`} prefetch>
       <Card className="bg-transparent hover:bg-card/50 hover:text-card-foreground transition-all ease-in duration-100 group border-0 shadow-none">
-        <CardHeader>
+        <CardHeader className="pb-4">
           <UserAuthorView profileIds={authorIds} />
-          <CardTitle className="text-3xl">{title}</CardTitle>
         </CardHeader>
-        <CardContent className="truncate text-sm line-clamp-3 overflow-auto">
+        <CardContent className="whitespace-break-spaces truncate text-sm line-clamp-3 overflow-auto">
           <Markdown content={content} />
         </CardContent>
         <CardFooter className="flex flex-row gap-4 text-sm text-muted-foreground">
