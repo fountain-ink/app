60ab88a798299252bd0b93991d45a12ab4e9043d
kualta <contact@kualta.dev>
2024-09-18 15:04:03 +0400

    restrict access to other users drafts

--- src/app/u/[user]/drafts/page.tsx
+++ src/app/u/[user]/drafts/page.tsx
@@ -1,13 +1,18 @@
 import { DraftsList } from "@/components/draft/draft-list";
+import ErrorPage from "@/components/ErrorPage";
 import { getAuthorizedClients } from "@/lib/get-auth-clients";
 
 const UserPage = async ({ params }: { params: { user: string } }) => {
-	const { lens } = await getAuthorizedClients();
+	const { lens, handle: userHandle } = await getAuthorizedClients();
 	const pageHandle = `lens/${params.user}`;
 	const profile = await lens.profile.fetch({ forHandle: pageHandle });
 
 	if (!profile) {
-		return null;
+		return <ErrorPage error="User not found" />;
+	}
+	
+	if (profile.id !== userHandle) {
+		return <ErrorPage error="Can't access other user's drafts" />;
 	}
 
 	return <DraftsList />;
