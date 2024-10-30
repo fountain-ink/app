b5a6d63e9c3b88ac81575d5574dcd35fa741caff
kualta <contact@kualta.dev>
2024-10-30 21:23:26 +0400

    add prose classes

--- src/app/writeplate/[id]/page.tsx
+++ src/app/writeplate/[id]/page.tsx
@@ -1,11 +1,14 @@
 import PlateEditor from "@/components/editor/plate-editor";
+import { proseClasses } from "@/styles/prose";
 
 export default async function WriteDraft({ params }: { params: { id: string } }) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center text-foreground bg-background">
       <div className="container flex flex-col items-center justify-center w-full max-w-lg md:max-w-xl lg:max-w-2xl">
         <div className="w-full min-h-screen py-4 my-20">
-          <PlateEditor />
+          <div className={proseClasses}>
+            <PlateEditor />
+          </div>
         </div>
       </div>
     </div>
