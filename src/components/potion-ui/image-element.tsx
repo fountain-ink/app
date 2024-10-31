9b9ecc565b2cb2fa4d7f37e3ac5d376da70da8f2
kualta <contact@kualta.dev>
2024-10-31 15:15:17 +0400

    update image placeholder

--- src/components/potion-ui/image-element.tsx
+++ src/components/potion-ui/image-element.tsx
@@ -1,74 +1,73 @@
-import React from 'react';
-import { cn, withRef } from '@udecode/cn';
-import { withHOC } from '@udecode/plate-common/react';
-import { Image, ImagePlugin, useMediaState } from '@udecode/plate-media/react';
-import { ResizableProvider, useResizableStore } from '@udecode/plate-resizable';
+import { cn, withRef } from "@udecode/cn";
+import { withHOC } from "@udecode/plate-common/react";
+import { Image, ImagePlugin, useMediaState } from "@udecode/plate-media/react";
+import { ResizableProvider, useResizableStore } from "@udecode/plate-resizable";
+import React from "react";
 
-import { Caption, CaptionTextarea } from './caption';
-import { MediaPopover } from './media-popover';
-import { PlateElement } from './plate-element';
-import {
-  mediaResizeHandleVariants,
-  Resizable,
-  ResizeHandle,
-} from './resizable';
+import { Caption, CaptionTextarea } from "./caption";
+import { MediaPopover } from "./media-popover";
+import { PlateElement } from "./plate-element";
+import { mediaResizeHandleVariants, Resizable, ResizeHandle } from "./resizable";
+
+const ImagePlaceholder = () => (
+  <div className="flex relative aspect-video w-full rounded-sm">
+    <div className="placeholder-background rounded-sm" />
+  </div>
+);
 
 export const ImageElement = withHOC(
   ResizableProvider,
-  withRef<typeof PlateElement>(
-    ({ children, className, nodeProps, ...props }, ref) => {
-      const { align = 'center', focused, readOnly, selected } = useMediaState();
+  withRef<typeof PlateElement>(({ children, className, nodeProps, ...props }, ref) => {
+    const { align = "center", focused, readOnly, selected } = useMediaState();
+    const width = useResizableStore().get.width();
+    const [isImageLoaded, setIsImageLoaded] = React.useState(false);
+    const showPlaceholder = !nodeProps?.url || !isImageLoaded;
 
-      const width = useResizableStore().get.width();
-
-      return (
-        <MediaPopover plugin={ImagePlugin}>
-          <PlateElement
-            ref={ref}
-            className={cn('py-2.5', className)}
-            {...props}
-          >
-            <figure className="group relative m-0" contentEditable={false}>
-              <Resizable
-                align={align}
-                options={{
-                  align,
-                  readOnly,
-                }}
-              >
-                <ResizeHandle
-                  className={mediaResizeHandleVariants({ direction: 'left' })}
-                  options={{ direction: 'left' }}
-                />
+    return (
+      <MediaPopover plugin={ImagePlugin}>
+        <PlateElement ref={ref} className={cn("py-2.5", className)} {...props}>
+          <figure className="group relative m-0" contentEditable={false}>
+            <Resizable
+              align={align}
+              options={{
+                align,
+                readOnly,
+              }}
+            >
+              <ResizeHandle
+                className={mediaResizeHandleVariants({ direction: "left" })}
+                options={{ direction: "left" }}
+              />
+              {showPlaceholder ? (
+                <ImagePlaceholder />
+              ) : (
                 <Image
                   className={cn(
-                    'block w-full max-w-full cursor-pointer object-cover px-0',
-                    'rounded-sm',
-                    focused && selected && 'ring-2 ring-ring ring-offset-2'
+                    "block w-full max-w-full cursor-pointer object-cover px-0",
+                    "rounded-sm",
+                    focused && selected && "ring-2 ring-ring ring-offset-2",
                   )}
                   alt=""
                   {...nodeProps}
+                  onLoad={() => setIsImageLoaded(true)}
                 />
-                <ResizeHandle
-                  className={mediaResizeHandleVariants({
-                    direction: 'right',
-                  })}
-                  options={{ direction: 'right' }}
-                />
-              </Resizable>
+              )}
+              <ResizeHandle
+                className={mediaResizeHandleVariants({
+                  direction: "right",
+                })}
+                options={{ direction: "right" }}
+              />
+            </Resizable>
 
-              <Caption style={{ width }} align={align}>
-                <CaptionTextarea
-                  readOnly={readOnly}
-                  placeholder="Write a caption..."
-                />
-              </Caption>
-            </figure>
+            <Caption style={{ width }} align={align}>
+              <CaptionTextarea readOnly={readOnly} placeholder="Write a caption..." />
+            </Caption>
+          </figure>
 
-            {children}
-          </PlateElement>
-        </MediaPopover>
-      );
-    }
-  )
+          {children}
+        </PlateElement>
+      </MediaPopover>
+    );
+  }),
 );
