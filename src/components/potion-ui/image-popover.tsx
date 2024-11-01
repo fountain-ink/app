7aa8aee342465628bbdae2ae93a3900fe4cd6098
kualta <contact@kualta.dev>
2024-11-01 12:40:03 +0400

    update image caption

--- src/components/potion-ui/image-popover.tsx
+++ src/components/potion-ui/image-popover.tsx
@@ -5,7 +5,9 @@ import { setNode, useEditorRef, useEditorSelector, useElement, useRemoveNodeButt
 import { UploadIcon } from "lucide-react";
 import React from "react";
 import { useReadOnly, useSelected } from "slate-react";
+import { LoadingSpinner } from "../loading-spinner";
 import { Button } from "./button";
+import { CaptionButton } from "./caption";
 import { Popover, PopoverAnchor, PopoverContent } from "./popover";
 import { Separator } from "./separator";
 
@@ -24,6 +26,7 @@ export function ImagePopover({ children, plugin }: ImagePopoverProps) {
 
   const selectionCollapsed = useEditorSelector((editor) => !isSelectionExpanded(editor), []);
   const isOpen = !readOnly && selected && selectionCollapsed;
+  const url = element?.url;
 
   const handleFileSelect = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -48,27 +51,21 @@ export function ImagePopover({ children, plugin }: ImagePopoverProps) {
 
       <PopoverContent sideOffset={0} className="w-auto p-1" onOpenAutoFocus={(e) => e.preventDefault()}>
         <div className="box-content flex h-9 items-center gap-1">
-          <div className="relative">
-            <input
-              type="file"
-              accept="image/*"
-              onChange={handleFileSelect}
-              className="absolute inset-0 cursor-pointer opacity-0"
-              disabled={isUploading}
-            />
-            <Button
-              size="sm"
-              variant="ghost"
-              disabled={isUploading}
-            >
-              {isUploading ? (
-                <Icons.spinner className="size-4 mr-2 animate-spin" />
-              ) : (
-                <UploadIcon className="size-4 mr-2" />
-              )}
-              Upload Image
-            </Button>
-          </div>
+          <Button size="sm" variant="ghost" disabled={isUploading}>
+            <div className="relative flex gap-1 items-center justify-center">
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleFileSelect}
+                className="absolute inset-0 cursor-pointer opacity-0"
+                disabled={isUploading}
+              />
+              {isUploading ? <LoadingSpinner /> : <>{!url && <UploadIcon className="size-4 mr-2" />}</>}
+              {url ? "Change" : "Upload"}
+            </div>
+          </Button>
+
+          <CaptionButton variant="ghost">Caption</CaptionButton>
 
           <Separator orientation="vertical" className="my-1" />
 
