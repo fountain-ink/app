23089400693f40d1104f2262fbfcd77d551c5105
kualta <contact@kualta.dev>
2024-11-02 20:14:59 +0400

    add collaboration server

---
+++ src/srv/collaboration.ts
@@ -0,0 +1,31 @@
+import { Logger } from "@hocuspocus/extension-logger";
+import { Server } from "@hocuspocus/server";
+import { slateNodesToInsertDelta } from "@slate-yjs/core";
+import * as Y from "yjs";
+
+const initialValue = [
+  {
+    type: "p",
+    children: [{ text: "" }],
+  },
+];
+
+const server = Server.configure({
+  port: 1234,
+  address: "0.0.0.0",
+
+  extensions: [new Logger()],
+
+  async onLoadDocument(data) {
+    if (data.document.isEmpty("content")) {
+      const insertDelta = slateNodesToInsertDelta(initialValue);
+      const sharedRoot = data.document.get("content", Y.XmlText);
+      sharedRoot.applyDelta(insertDelta);
+    }
+
+    return data.document;
+  },
+});
+
+server.enableMessageLogging();
+server.listen();
