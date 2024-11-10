3631edab1c22afff493728c1c2758f40089f03b5
kualta <contact@kualta.dev>
2024-11-10 11:51:17 +0400

    update header

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useScroll } from "@/hooks/use-scroll";
+import { motion, useSpring } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { FountainLogo } from "../custom-icons";
@@ -14,38 +15,53 @@ export const Header = () => {
   const isVisible = useScroll();
   const hostname = typeof window !== "undefined" && window.location.hostname ? window.location.hostname : "";
 
+  const springConfig = {
+    stiffness: 300,
+    damping: 20,
+  };
+
+  const y = useSpring(0, springConfig);
+
+  if (!isVisible) {
+    y.set(-100);
+  } else {
+    y.set(0);
+  }
+
   // FIXME: Temporary before release
   if (!hostname.includes("dev") && !hostname.includes("localhost")) {
     return (
-      <div
-        className={`fixed top-0 w-full p-2 z-[40] flex justify-between items-center pointer-events-none transition-opacity duration-500 ${
-          isVisible ? "opacity-100" : "opacity-0"
-        } hover:opacity-100`}
+      <motion.div
+        style={{ y }}
+        className="fixed top-0 w-full h-[100px] -mt-[42px] pt-[50px] z-[40] bg-background/20 backdrop-blur-md border-b border-border overflow-hidden p-2"
       >
-        <Link href={"/"} className="w-10 h-10 flex items-center justify-center pointer-events-auto">
-          <FountainLogo />
-        </Link>
-      </div>
+        <div className="flex items-end justify-between absolute bg-gradient-to-t from-transparent to-background bottom-0 left-0 right-0 h-[80px] pb-2 px-2">
+          <Link href={"/"} className="w-10 h-10 flex items-center justify-center pointer-events-auto">
+            <FountainLogo />
+          </Link>
+        </div>
+      </motion.div>
     );
   }
 
   const isWritePage = pathname.startsWith("/write");
 
   return (
-    <div
-      className={`fixed top-0 w-full p-2 z-[40] flex justify-between items-center backdrop-blur-xl bg-background/40 border-b border-border transition-opacity duration-500 ${
-        isVisible ? "opacity-100" : "opacity-0"
-      } hover:opacity-100`}
+    <motion.div
+      style={{ y }}
+      className="fixed top-0 w-full h-[100px] -mt-[42px] pt-[50px] z-[40] bg-background/20 backdrop-blur-md border-b border-border overflow-hidden p-2"
     >
-      <Link href={"/"} className="w-10 h-10 flex items-center justify-center pointer-events-auto">
-        <FountainLogo />
-      </Link>
-      <div className="flex gap-4 pointer-events-auto">
-        {isWritePage && <ThemeSidebar />}
-        {isWritePage && <PublishMenu />}
-        {!isWritePage && <WriteMenu />}
-        <UserMenu />
+      <div className="flex items-end justify-between absolute bg-gradient-to-t from-transparent to-background bottom-0 left-0 right-0 h-[80px] pb-2 px-2">
+        <Link href={"/"} className="w-10 h-10 flex items-center justify-center pointer-events-auto">
+          <FountainLogo />
+        </Link>
+        <div className="flex gap-4 pointer-events-auto">
+          {isWritePage && <ThemeSidebar />}
+          {isWritePage && <PublishMenu />}
+          {!isWritePage && <WriteMenu />}
+          <UserMenu />
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
