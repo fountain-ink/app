c4bed5db02e7a60983b6e4b7527ff9566dc1272b
kualta <contact@kualta.dev>
2024-11-16 15:00:04 +0400

    update normalization

--- src/components/editor/plate-normalization.ts
+++ src/components/editor/plate-normalization.ts
@@ -1,7 +1,7 @@
-import { type TElement, insertNodes, isEditor, removeNodes } from "@udecode/plate-common";
+import { type TElement, getNodeString, insertEmptyElement, isEditor, removeNodes } from "@udecode/plate-common";
+import type { PlateEditor } from "@udecode/plate-common/react";
 import { createPlatePlugin } from "@udecode/plate-common/react";
 import { HEADING_KEYS } from "@udecode/plate-heading";
-import { Node } from "slate";
 
 export interface NormalizePluginOptions {
   requireH1?: boolean;
@@ -13,76 +13,74 @@ const DEFAULT_OPTIONS: NormalizePluginOptions = {
   requireH2: false,
 };
 
-const initial = { type: "p", children: [{ text: "" }] };
-// const matchesInitial = (node: TElement) =>
-//   Node.matches(node, initial) && node.children.length === 1 && node.children[0]?.text == "";
+export function ensureLeadingBlock(editor: PlateEditor, { event }: { event?: React.MouseEvent } = {}) {
+  const { children } = editor;
+
+  if (children.length === 0) {
+    // Don't try to select an empty editor
+    event?.preventDefault();
+    event?.stopPropagation();
+
+    insertEmptyElement(editor, HEADING_KEYS.h1, { select: true });
+  } else if (children?.[0]?.type !== HEADING_KEYS.h1) {
+    insertEmptyElement(editor, HEADING_KEYS.h1, {
+      at: [0],
+      select: true,
+    });
+  }
+}
+
+const allowDelete = (editor: PlateEditor) => {
+  const path = editor.selection?.anchor.path;
+  const offset = editor.selection?.anchor.offset;
+
+  if (path !== undefined && offset !== undefined && offset === 0 && path[0] === 0 && path[1] === 0) {
+    return false;
+  }
+
+  return true;
+};
 
 export const NormalizePlugin = createPlatePlugin({
   key: "normalize",
   options: DEFAULT_OPTIONS,
 
   extendEditor: ({ editor, useOption }) => {
-    const { normalizeNode } = editor;
+    const { deleteBackward, deleteForward } = editor;
+
+    editor.deleteForward = (...args) => {
+      if (allowDelete(editor)) {
+        return deleteForward(...args);
+      }
+
+      const node = editor.children[0];
+
+      if (node && getNodeString(node).length > 0) {
+        return deleteForward(...args);
+      }
+    };
+
+    editor.deleteBackward = (...args) => {
+      if (allowDelete(editor)) {
+        return deleteBackward(...args);
+      }
+    };
 
     editor.normalizeNode = (entry) => {
       const [node, path] = entry;
 
+      ensureLeadingBlock(editor);
+
       if (isEditor(node)) {
         const nodes = node.children as TElement[];
-        let normalized = false;
-
-        console.log(nodes);
-
-        // if (nodes.length > 1 && nodes[0] && matchesInitial(nodes[0]) && nodes[1] && matchesInitial(nodes[1])) {
-        //   console.log("REMOVE INTIAL");
-        //   return removeNodes(editor, { at: [0, 1] });
-        // }
-
-        // if (nodes.length > 1 && nodes[0] && !matchesInitial(nodes[0]) && nodes[1] && matchesInitial(nodes[1])) {
-        //   console.log("INSRET INTIAL");
-        //   return insertNodes(editor, initial, { at: [0] });
-        // }
-
-        // Ensure first node is h1
-        if (!normalized && (nodes.length === 0 || nodes[0]?.type !== HEADING_KEYS.h1)) {
-          console.log("ENSURE H1");
-          const existingContent = nodes[0]?.children || [{ text: "" }];
-          if (nodes.length > 0) {
-            removeNodes(editor, { at: [0] });
-          }
-          insertNodes(editor, { type: HEADING_KEYS.h1, children: existingContent }, { at: [0] });
-          normalized = true;
-        }
-
-        // Ensure second node is h2
-        if (!normalized && (nodes.length < 2 || nodes[1]?.type !== HEADING_KEYS.h2)) {
-          console.log("ENSURE H2");
-          const existingContent = nodes[1]?.children || [{ text: "" }];
-          if (nodes.length > 1) {
-            removeNodes(editor, { at: [1] });
-          }
-          insertNodes(editor, { type: HEADING_KEYS.h2, children: existingContent }, { at: [1] });
-          normalized = true;
-        }
 
         // Remove any additional h1 or h2 nodes
-        if (!normalized) {
-          console.log("ENSURE REMOVE");
-          for (let i = 2; i < nodes.length; i++) {
-            if (nodes[i]?.type === HEADING_KEYS.h1 || nodes[i]?.type === HEADING_KEYS.h2) {
-              removeNodes(editor, { at: [i] });
-              normalized = true;
-              break;
-            }
+        for (let i = 1; i < nodes.length; i++) {
+          if (nodes[i]?.type === HEADING_KEYS.h1 || nodes[i]?.type === HEADING_KEYS.h2) {
+            removeNodes(editor, { at: [i] });
+            break;
           }
         }
-
-      //   if (!normalized) {
-      //     normalizeNode(entry);
-      //   }
-      // } else {
-      //   normalizeNode(entry);
-      // }
       }
     };
 
