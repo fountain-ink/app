dbfe71e971b9f7a1e092c3bd30bc13b7645ca7ff
kualta <contact@kualta.dev>
2024-11-18 19:59:26 +0400

    remove color plugins

--- src/components/editor/plate-plugins.tsx
+++ src/components/editor/plate-plugins.tsx
@@ -11,7 +11,6 @@ import { DatePlugin } from "@udecode/plate-date/react";
 import { DndPlugin } from "@udecode/plate-dnd";
 import { DocxPlugin } from "@udecode/plate-docx";
 import { EmojiPlugin } from "@udecode/plate-emoji/react";
-import { FontBackgroundColorPlugin, FontColorPlugin } from "@udecode/plate-font/react";
 import { HEADING_KEYS, HEADING_LEVELS } from "@udecode/plate-heading";
 import { HeadingPlugin, TocPlugin } from "@udecode/plate-heading/react";
 import { HighlightPlugin } from "@udecode/plate-highlight/react";
@@ -291,8 +290,6 @@ export const staticPlugins = [
 
   // Marks
   BasicMarksPlugin,
-  FontColorPlugin,
-  FontBackgroundColorPlugin,
   HighlightPlugin,
   KbdPlugin,
 
