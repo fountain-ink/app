347971f77b57b36c206a230d04f66d6189f67c20
kualta <contact@kualta.dev>
2024-12-01 13:04:50 +0400

    update drafts

--- src/app/u/[user]/(index)/drafts/page.tsx
+++ src/app/u/[user]/(index)/drafts/page.tsx
@@ -1,21 +1,22 @@
 import { DraftsList } from "@/components/draft/draft-list";
+import { CloudDraftsList } from "@/components/draft/draft-list-cloud";
 import ErrorPage from "@/components/error-page";
 import { getAuthWithCookies } from "@/lib/auth/get-auth-clients";
 
 const UserPage = async ({ params }: { params: { user: string } }) => {
-  const { lens, handle: userHandle } = await getAuthWithCookies();
-  const pageHandle = `lens/${params.user}`;
-  const profile = await lens.profile.fetch({ forHandle: pageHandle });
-
-  if (!profile) {
-    return <ErrorPage error="User not found" />;
-  }
+  const { handle: userHandle, profileId } = await getAuthWithCookies();
 
   if (params.user !== userHandle) {
     return <ErrorPage error="Can't access other user's drafts" />;
   }
 
-  return <DraftsList />;
+  return  <div className="flex flex-col items-center justify-center ">
+    <div className="flex flex-col grow items-stretch justify-center w-full max-w-3xl sm:max-w-3xl">
+      <h1 className="text-4xl font-bold text-center m-8">Drafts</h1>
+      <CloudDraftsList profileId={profileId} />
+    </div>
+  </div>
+;
 };
 
 export default UserPage;
--- src/components/post/article-layout.tsx
+++ src/components/post/article-layout.tsx
@@ -6,7 +6,7 @@ export const ArticleLayout = async ({ children }: { children: React.ReactNode })
   return (
     <SmoothScroll>
       <article
-        className="overflow-y-auto w-screen h-screen article relative [&_.slate-selection-area]:border [&_.slate-selection-area]:border-primary [&_.slate-selection-area]:bg-primary/10 "
+        className="overflow-y-auto w-screen h-screen article relative pt-20 [&_.slate-selection-area]:border [&_.slate-selection-area]:border-primary [&_.slate-selection-area]:bg-primary/10 "
         data-plate-selectable="true"
         id="scroll_container"
       >
