2ffacba8f24743bd37fb0aa34b987b1667931b2f
kualta <contact@kualta.dev>
2024-12-06 13:32:07 +0400

    add copy to codeblocks

--- src/components/ui/code-block-element.tsx
+++ src/components/ui/code-block-element.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useCopyToClipboard } from "@/hooks/use-copy-to-clipboard";
 import { cn, withRef } from "@udecode/cn";
 import { useCodeBlockElementState } from "@udecode/plate-code-block/react";
-import { setNode, useEditorRef, useRemoveNodeButton } from "@udecode/plate-common/react";
+import { getNodeString } from "@udecode/plate-common";
+import { setNode, useEditorRef } from "@udecode/plate-common/react";
 import { useMediaState } from "@udecode/plate-media/react";
 import { useState } from "react";
 import { useReadOnly } from "slate-react";
+import { Button } from "./button";
 import { Caption, CaptionTextarea } from "./caption";
 import { CodeBlockCombobox } from "./code-block-combobox";
 import "./code-block-element.css";
@@ -17,9 +20,9 @@ export const CodeBlockElement = withRef<typeof PlateElement>(({ children, classN
   const { element } = props;
   const state = useCodeBlockElementState({ element });
   const { focused, selected } = useMediaState();
-  const { props: removeButtonProps } = useRemoveNodeButton({ element });
   const editor = useEditorRef();
   const [width, setWidth] = useState<ElementWidth>((element?.width as ElementWidth) || "column");
+  const { copyToClipboard } = useCopyToClipboard();
   const readonly = useReadOnly();
 
   const handleWidth = (newWidth: ElementWidth) => {
@@ -27,29 +30,45 @@ export const CodeBlockElement = withRef<typeof PlateElement>(({ children, classN
     setNode(editor, element, { width: newWidth });
   };
 
-  const popoverContent = <CodeBlockCombobox />;
+  const popoverContent = (
+    <>
+      <CodeBlockCombobox />
+
+      <Button
+        size="default"
+        variant={"ghost"}
+        onClick={() => {
+          const lines = element.children.map((child) => getNodeString(child));
+          copyToClipboard(lines.join("\n\n"), {
+            tooltip: "Copied code to clipboard",
+          });
+        }}
+      >
+        Copy
+      </Button>
+    </>
+  );
 
   return (
     <ElementPopover onWidthChange={handleWidth} content={popoverContent}>
       <PlateElement
         ref={ref}
-        className={cn(
-          "relative my-8 bg-muted text-foreground rounded-sm",
-          width && ELEMENT_WIDTH_CLASSES[width],
-          state.className,
-          className,
-        )}
+        className={cn("relative my-8 rounded-sm", width && ELEMENT_WIDTH_CLASSES[width], state.className, className)}
         {...props}
       >
         <figure className="group">
           <ScrollArea
             orientation="horizontal"
-            className={cn("rounded-sm overflow-hidden", focused && selected && "ring-2 ring-ring")}
+            className={cn(
+              "rounded-sm bg-muted text-foreground  overflow-hidden",
+              focused && selected && "ring-2 ring-ring",
+            )}
           >
             <pre className="bg-muted px-6 py-4 text-foreground/80 font-mono text-sm not-prose leading-[normal] [tab-size:2] min-w-full">
               <code>{children}</code>
             </pre>
           </ScrollArea>
+
           <Caption className={width} align="center" contentEditable={false}>
             <CaptionTextarea readOnly={readonly} placeholder="Write a caption..." />
           </Caption>
