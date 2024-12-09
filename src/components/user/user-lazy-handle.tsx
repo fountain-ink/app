55032e322b9af8e0e46fa71483f777f7247c27c2
kualta <contact@kualta.dev>
2024-12-09 20:35:33 +0400

    add underlines to lazy handles

--- src/components/user/user-lazy-handle.tsx
+++ src/components/user/user-lazy-handle.tsx
@@ -6,7 +6,12 @@ import { UserCard } from "./user-card";
 export const UserLazyHandle = ({ handle }: { handle: string }) => {
   return (
     <UserCard handle={handle}>
-      <Link onClick={(e) => e.stopPropagation()} href={`/u/${handle}`} prefetch>
+      <Link
+        className="underline hover:no-underline decoration-2 underline-offset-4"
+        onClick={(e) => e.stopPropagation()}
+        href={`/u/${handle}`}
+        prefetch
+      >
         @{handle}
       </Link>
     </UserCard>
