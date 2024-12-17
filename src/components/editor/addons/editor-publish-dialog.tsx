b1ea4e3170cd4fb6510e51951e77cafd3176deed
kualta <contact@kualta.dev>
2024-12-17 17:34:43 +0400

    remove preview tab

--- src/components/editor/addons/editor-publish-dialog.tsx
+++ src/components/editor/addons/editor-publish-dialog.tsx
@@ -1,70 +1,56 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { usePublishStore } from "@/hooks/use-publish-store";
-import { usePlateStore } from "@udecode/plate-common/react";
 import { AnimatePresence, motion } from "framer-motion";
-import { Eye, PenIcon, ShoppingBag } from "lucide-react";
-import { useEffect, useState } from "react";
-import { PublishButton } from "./editor-publish-button";
+import { PenIcon, ShoppingBag } from "lucide-react";
+import { useState } from "react";
 import { CollectingTab } from "./publish-tabs/collecting-tab";
-import { PreviewTab } from "./publish-tabs/preview-tab";
 import { DetailsTab } from "./publish-tabs/details-tab";
 
 export const PublishDialog = () => {
   const { isOpen, setIsOpen } = usePublishStore();
   const [tab, setTab] = useState("article");
-  const isPreview = tab === "preview";
-  const setReadOnly = usePlateStore().set.readOnly();
-
-  useEffect(() => {
-    setReadOnly(isPreview && isOpen);
-  }, [isPreview, isOpen]);
 
   return (
     <AnimatePresence>
       {isOpen && (
         <>
-          {!isPreview && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              className="fixed inset-0 bg-background z-[100]"
-              onClick={() => setIsOpen(false)}
-            />
-          )}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="fixed inset-0 bg-background z-[100]"
+            onClick={() => setIsOpen(false)}
+          />
           {isOpen && (
             <motion.div
               initial={{
                 opacity: 0,
                 scale: 0.95,
                 position: "fixed",
-                top: isPreview ? "auto" : "50%",
+                top: "50%",
                 left: "50%",
-                bottom: isPreview ? "0" : "auto",
                 width: "min(600px, 95vw)",
                 x: "-50%",
-                y: isPreview ? "100%" : "-50%",
+                y: "-50%",
               }}
               animate={{
                 opacity: 1,
                 scale: 1,
-                height: isPreview ? "200px" : "min(800px, 80vh)",
+                height: "min(800px, 80vh)",
                 position: "fixed",
-                top: isPreview ? "auto" : "50%",
+                top: "50%",
                 left: "50%",
-                bottom: isPreview ? "10px" : "auto",
                 width: "min(600px, 95vw)",
                 x: "-50%",
-                y: isPreview ? 0 : "-50%",
+                y: "-50%",
               }}
               exit={{
                 opacity: 0,
                 scale: 0.95,
-                y: isPreview ? "100%" : "-50%",
+                y: "-50%",
               }}
               transition={{
                 type: "spring",
@@ -77,14 +63,12 @@ export const PublishDialog = () => {
               <div className="p-6 h-full w-full flex flex-col">
                 <div className="flex-shrink-0">
                   <div className="text-lg font-semibold mb-6">
-                    {tab === "preview" && "Preview Article"}
-                    {tab === "collecting" && "Article Collecting"}
-                    {tab === "article" && "Article Details"}
+                    {tab === "collecting" ? "Article Collecting" : "Article Details"}
                   </div>
                 </div>
 
                 <Tabs value={tab} onValueChange={setTab} className="flex-1 flex flex-col">
-                  <TabsList className="grid w-fit grid-cols-3 mx-auto max-w-full flex-shrink-0">
+                  <TabsList className="grid w-fit grid-cols-2 max-w-full flex-shrink-0">
                     <TabsTrigger value="article" className="flex items-center gap-2">
                       <PenIcon className="w-4 h-4" />
                       Details
@@ -93,10 +77,6 @@ export const PublishDialog = () => {
                       <ShoppingBag className="w-4 h-4" />
                       Collecting
                     </TabsTrigger>
-                    <TabsTrigger value="preview" className="flex items-center gap-2">
-                      <Eye className="w-4 h-4" />
-                      Preview
-                    </TabsTrigger>
                   </TabsList>
                   <ScrollArea className="flex-1 -mx-6">
                     <TabsContent value="article" className="space-y-4 px-6">
@@ -104,9 +84,6 @@ export const PublishDialog = () => {
                     </TabsContent>
                     <TabsContent value="collecting" className="space-y-6 px-6">
                       <CollectingTab />
-                    </TabsContent>
-                    <TabsContent value="preview" className="space-y-4 px-6">
-                      <PreviewTab />
                     </TabsContent>
                   </ScrollArea>
                 </Tabs>
--- src/components/editor/addons/publish-tabs/preview-tab.tsx
+++
@@ -1,3 +0,0 @@
-export const PreviewTab = () => {
-  return <div className="h-full" />;
-};
