f43eec262e441b02f1b01bb078ba386e064a614f
kualta <contact@kualta.dev>
2024-12-18 16:11:50 +0400

    update share modal

--- src/components/draft/draft-share-modal.tsx
+++ src/components/draft/draft-share-modal.tsx
@@ -2,8 +2,11 @@
 
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { LinkIcon } from "lucide-react";
 import { useState } from "react";
 import { toast } from "sonner";
+import { Badge } from "../ui/badge";
 
 interface DraftShareModalProps {
   isOpen: boolean;
@@ -11,22 +14,18 @@ interface DraftShareModalProps {
 }
 
 export const DraftShareModal = ({ isOpen, onClose }: DraftShareModalProps) => {
-  const [isCopying, setIsCopying] = useState(false);
+  const [viewAccess, setViewAccess] = useState<"everyone" | "invited">("everyone");
 
   const copyLink = async () => {
     try {
-      setIsCopying(true);
       await navigator.clipboard.writeText(window.location.href);
       toast.success("Link copied!", { description: "The draft link has been copied to your clipboard." });
     } catch (error) {
       toast.error("Failed to copy", {
         description: "There was an error copying the link.",
       });
-    } finally {
-      setIsCopying(false);
     }
   };
-
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
@@ -35,10 +34,31 @@ export const DraftShareModal = ({ isOpen, onClose }: DraftShareModalProps) => {
           <DialogTitle>Share draft</DialogTitle>
         </DialogHeader>
         <div className="space-y-4">
-          <p className="text-sm text-muted-foreground">Anyone with this link will be able to view your draft post.</p>
-          <Button onClick={copyLink} disabled={isCopying} className="w-full">
-            {isCopying ? "Copying..." : "Copy link"}
-          </Button>
+          <div className="flex items-center place-content-between gap-3">
+            <Select value={viewAccess} onValueChange={(value: "everyone" | "invited") => setViewAccess(value)}>
+              <SelectTrigger className="w-[280px]">
+                <SelectValue placeholder="Select view access" />
+              </SelectTrigger>
+              <SelectContent className="z-[60]">
+                <SelectItem value="everyone">Everyone with link</SelectItem>
+                <SelectItem disabled value="invited">
+                  Only invited users{" "}
+                  <Badge className="ml-2" variant={"outline"}>
+                    soon
+                  </Badge>
+                </SelectItem>
+              </SelectContent>
+            </Select>
+            <Button onClick={copyLink} variant={"ghost"}>
+              <LinkIcon size={18} />
+              {"Copy link"}
+            </Button>
+          </div>
+          <p className="text-sm text-muted-foreground">
+            {viewAccess === "everyone"
+              ? "Anyone with this link will be able to edit your draft."
+              : "Only users you specifically invite will be able to edit this draft."}
+          </p>
         </div>
       </DialogContent>
     </Dialog>
