a4e09d7bab744bdefe40486a32ab052b22c4f97c
kualta <contact@kualta.dev>
2024-12-18 13:30:04 +0400

    update settings modal

--- src/components/settings/settings-profile-modal.tsx
+++ src/components/settings/settings-profile-modal.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -43,22 +43,30 @@ export function ProfileSettingsModal({ profile, trigger, open, onOpenChange }: P
 
   if (!profile) return null;
 
+  const [isUploading, setIsUploading] = useState(false);
+
   const handleSave = useCallback(async () => {
     let picture: any;
     let coverPictureUri: any;
 
-    if (profilePicture) {
-      const formData = new FormData();
-      formData.append("file", profilePicture);
-      formData.append("handle", handle);
-      picture = await uploadFileFormData(formData);
-    }
+    if (profilePicture || coverPicture) {
+      setIsUploading(true);
 
-    if (coverPicture) {
-      const formData = new FormData();
-      formData.append("file", coverPicture);
-      formData.append("handle", handle);
-      coverPictureUri = await uploadFileFormData(formData);
+      if (profilePicture) {
+        const formData = new FormData();
+        formData.append("file", profilePicture);
+        formData.append("handle", handle);
+        picture = await uploadFileFormData(formData);
+      }
+
+      if (coverPicture) {
+        const formData = new FormData();
+        formData.append("file", coverPicture);
+        formData.append("handle", handle);
+        coverPictureUri = await uploadFileFormData(formData);
+      }
+
+      setIsUploading(false);
     }
 
     const attributes = [
@@ -97,6 +105,7 @@ export function ProfileSettingsModal({ profile, trigger, open, onOpenChange }: P
     autoPublish,
     saveSettings,
     onOpenChange,
+    setIsUploading,
   ]);
 
   return (
@@ -108,58 +117,53 @@ export function ProfileSettingsModal({ profile, trigger, open, onOpenChange }: P
           <DialogDescription>Customize your profile preferences.</DialogDescription>
         </DialogHeader>
 
-          <div className="space-y-4">
-            <div className="relative">
+        <div className="space-y-4">
+          <div className="relative">
+            <ImageUploader
+              label="Cover Picture"
+              initialImage={currentMetadata?.coverPicture?.raw?.uri || ""}
+              aspectRatio={3}
+              onImageChange={setCoverPicture}
+            />
+            <div className="absolute bottom-0 translate-y-1/2 left-8 z-10">
               <ImageUploader
-                label="Cover Picture"
-                initialImage={currentMetadata?.coverPicture?.raw?.uri || ""}
-                aspectRatio={3}
-                onImageChange={setCoverPicture}
+                label="Avatar"
+                initialImage={
+                  currentMetadata?.picture?.__typename === "ImageSet"
+                    ? currentMetadata.picture.raw?.uri || ""
+                    : currentMetadata?.picture?.image?.raw?.uri || ""
+                }
+                aspectRatio={1}
+                onImageChange={setProfilePicture}
               />
-              <div className="absolute bottom-0 translate-y-1/2 left-8 z-10">
-                <ImageUploader
-                  label="Avatar"
-                  initialImage={
-                    currentMetadata?.picture?.__typename === "ImageSet"
-                      ? currentMetadata.picture.raw?.uri || ""
-                      : currentMetadata?.picture?.image?.raw?.uri || ""
-                  }
-                  aspectRatio={1}
-                  onImageChange={setProfilePicture}
-                />
-              </div>
             </div>
-            {/* Spacer to account for overlapping avatar */}
-            <div className="h-20" />
           </div>
-          {/* <div className="space-y-2">
-            <Label htmlFor="username">Username</Label>
-            <Input id="username" placeholder={handle} disabled />
-          </div> */}
-          <div className=" space-y-2">
-            <Label htmlFor="profile-title">Name</Label>
-            <Input
-              id="profile-title"
-              value={profileTitle}
-              onChange={(e) => setProfileTitle(e.target.value)}
-              placeholder="Your profile title"
-            />
-          </div>
-          <div className="flex flex-col gap-2 relative">
-            <Label htmlFor="profile-description">Bio</Label>
-            <TextareaAutosize
-              id="profile-description"
-              value={profileDescription}
-              variant={"default"}
-              className="p-2"
-              onChange={(e) => setProfileDescription(e.target.value)}
-              placeholder="Your profile description"
-            />
-          </div>
+          <div className="h-20" />
+        </div>
+        <div className=" space-y-2">
+          <Label htmlFor="profile-title">Name</Label>
+          <Input
+            id="profile-title"
+            value={profileTitle}
+            onChange={(e) => setProfileTitle(e.target.value)}
+            placeholder="Your profile title"
+          />
+        </div>
+        <div className="flex flex-col gap-2 relative">
+          <Label htmlFor="profile-description">Bio</Label>
+          <TextareaAutosize
+            id="profile-description"
+            value={profileDescription}
+            variant={"default"}
+            className="p-2"
+            onChange={(e) => setProfileDescription(e.target.value)}
+            placeholder="Your profile description"
+          />
+        </div>
 
-          <Button onClick={handleSave} disabled={isSavingProfileSettings}>
-            {isSavingProfileSettings ? "Saving..." : "Save Settings"}
-          </Button>
+        <Button onClick={handleSave} disabled={isUploading || isSavingProfileSettings}>
+          {isUploading ? "Uploading..." : isSavingProfileSettings ? "Saving..." : "Save Settings"}
+        </Button>
       </DialogContent>
     </Dialog>
   );
