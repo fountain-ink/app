b1e5c77abeceeab6169450f775e99a077f56eb75
kualta <contact@kualta.dev>
2024-12-27 08:52:20 +0400

    update cookie manager

--- src/components/auth/cookie-manager.tsx
+++ src/components/auth/cookie-manager.tsx
@@ -1,11 +1,35 @@
 "use client";
 
-import { useRefreshToken } from "@lens-protocol/react-web";
-import { setCookie } from "cookies-next";
+import { SessionType, useRefreshToken, useSession } from "@lens-protocol/react-web";
+import { getCookie, setCookie } from "cookies-next";
 import { useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const generateGuestId = () => `guest_${uuidv4()}`;
 
 export const CookieManager = () => {
   const refreshToken = useRefreshToken();
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const existingUserId = getCookie("profileId");
+
+    if (!existingUserId) {
+      setCookie("profileId", generateGuestId(), {
+        maxAge: 365 * 24 * 60 * 60, // 1 year
+        sameSite: "lax",
+        path: "/",
+      });
+    }
+
+    if (session?.type === SessionType.WithProfile) {
+      setCookie("profileId", session.profile.id, {
+        maxAge: 365 * 24 * 60 * 60, // 1 year
+        sameSite: "lax",
+        path: "/",
+      });
+    }
+  }, [session]);
 
   useEffect(() => {
     if (refreshToken) {
