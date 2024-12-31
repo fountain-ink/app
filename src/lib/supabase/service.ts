42b09be0a7c8533ef35f846aa6ef693db9d910b7
kualta <contact@kualta.dev>
2024-12-31 20:58:25 +0400

    dont use cookies for service client

--- src/lib/supabase/service.ts
+++ src/lib/supabase/service.ts
@@ -1,32 +1,11 @@
-import { env } from "@/env";
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 import { Database } from "./database";
+import { env } from "@/env.server";
+import { createClient } from "@supabase/supabase-js";
 
 export async function createServiceClient() {
-  const cookieStore = cookies();
-
-  const supabase = createServerClient<Database>(env.NEXT_PUBLIC_SUPABASE_URL, env.SUPABASE_SERVICE_KEY, {
-    cookies: {
-      getAll() {
-        return cookieStore.getAll();
-      },
-      setAll(cookiesToSet) {
-        try {
-          for (const { name, value, options } of cookiesToSet) {
-            cookieStore.set(name, value, options);
-          }
-        } catch {
-          // The `setAll` method was called from a Server Component.
-          // This can be ignored if you have middleware refreshing
-          // user sessions.
-        }
-      },
-    },
-    auth: {
-      detectSessionInUrl: true,
-      persistSession: true,
-    },
+  const supabase = createClient<Database>(env.NEXT_PUBLIC_SUPABASE_URL, env.SUPABASE_SERVICE_KEY, {
     global: {
       headers: {
         "x-application-name": "fountain",
