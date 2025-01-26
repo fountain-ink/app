dbb3810c65a9481bdd85802de77c2977f177f8be
kualta <contact@kualta.dev>
2025-01-26 19:22:57 +0400

    update code leaf

--- src/components/ui/code-leaf.tsx
+++ src/components/ui/code-leaf.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import React from "react";
-
 import { cn, withRef } from "@udecode/cn";
 import { PlateLeaf } from "@udecode/plate-common/react";
 
@@ -10,7 +8,10 @@ export const CodeLeaf = withRef<typeof PlateLeaf>(({ children, className, ...pro
     <PlateLeaf
       ref={ref}
       asChild
-      className={cn("whitespace-pre-wrap rounded-md bg-muted px-[0.3em] py-[0.2em] font-mono text-sm", className)}
+      className={cn(
+        "whitespace-pre-wrap rounded-[4px] border border-secondary/60 bg-muted px-[0.3em] py-[0.2em] font-mono text-sm",
+        className,
+      )}
       {...props}
     >
       <code>{children}</code>
