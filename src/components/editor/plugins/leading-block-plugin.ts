151ae2faaf8d471601c5834675bf0cd24dc5f514
kualta <contact@kualta.dev>
2025-02-12 11:07:10 +0400

    add leading block plugin

---
+++ src/components/editor/plugins/leading-block-plugin.ts
@@ -0,0 +1,54 @@
+import {
+  type OverrideEditor,
+  type PluginConfig,
+  type QueryNodeOptions,
+  createTSlatePlugin,
+  queryNode,
+} from "@udecode/plate";
+import { TitlePlugin } from "./title-plugin";
+
+export type LeadingBlockConfig = PluginConfig<
+  "leadingBlock",
+  {
+    /** Level where the leading node should be, the first level being 0. */
+    level?: number;
+    /** Type of the leading block */
+    type?: string;
+  } & QueryNodeOptions
+>;
+
+/**
+ * Add a leading block when the first node type is not `type`
+ */
+const withLeadingBlock: OverrideEditor<LeadingBlockConfig> = ({ editor, getOptions, tf: { normalizeNode } }) => ({
+  transforms: {
+    normalizeNode([currentNode, currentPath]) {
+      const { level, type, ...query } = getOptions();
+
+      if (currentPath.length === 0) {
+        const firstChild = editor.children[0];
+
+        if (!firstChild || (firstChild.type !== type)) {
+          editor.tf.insertNodes(editor.api.create.block({ type }), { at: [0] });
+          return;
+        }
+      }
+
+      return normalizeNode([currentNode, currentPath]);
+    },
+  },
+});
+
+/** @see {@link withLeadingBlock} */
+export const LeadingBlockPlugin = createTSlatePlugin<LeadingBlockConfig>({
+  key: "leadingBlock",
+  options: {
+    level: 0,
+  },
+})
+  .overrideEditor(withLeadingBlock)
+  .extend(({ editor }) => ({
+    options: {
+      type: editor.getType(TitlePlugin),
+    },
+  }));
