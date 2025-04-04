2e73b8e6d509208fb3f87904d1f736fde6ed2492
kualta <contact@kualta.dev>
2025-04-04 19:26:56 +0400

    conditionally show collect button

--- src/components/post/post-reactions.tsx
+++ src/components/post/post-reactions.tsx
@@ -8,6 +8,8 @@ export const PostReactions = ({ post }: { post: Post }) => {
   const { handleLike, handleComment, handleCollect, isCollectSheetOpen, handleCollectSheetOpenChange } =
     usePostActions(post);
 
+
+  const canCollect = post.operations?.canSimpleCollect.__typename === "SimpleCollectValidationPassed";
   return (
     <div className="flex flex-row gap-3 items-center justify-center">
       <ActionButton
@@ -20,15 +22,17 @@ export const PostReactions = ({ post }: { post: Post }) => {
         isActive={post.operations?.hasCommented.optimistic}
         shouldIncrementOnClick={false}
       />
-      <ActionButton
-        icon={ShoppingBag}
-        label="Collect"
-        initialCount={post.stats.collects}
-        strokeColor="rgb(254,178,4)"
-        fillColor="rgba(254, 178, 4, 0.3)"
-        onClick={handleCollect}
-        shouldIncrementOnClick={false}
-      />
+      {canCollect && (
+        <ActionButton
+          icon={ShoppingBag}
+          label="Collect"
+          initialCount={post.stats.collects}
+          strokeColor="rgb(254,178,4)"
+          fillColor="rgba(254, 178, 4, 0.3)"
+          onClick={handleCollect}
+          shouldIncrementOnClick={false}
+        />
+      )}
       <ActionButton
         icon={Heart}
         label="Like"
