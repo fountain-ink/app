0c6e2c97badfce448a4dee8f69c53dc37d6fd27a
kualta <contact@kualta.dev>
2025-04-04 15:21:02 +0400

    update lens cookies max age

--- src/lib/lens/storage.ts
+++ src/lib/lens/storage.ts
@@ -1,6 +1,8 @@
 import { IStorageProvider } from "@lens-protocol/client";
 import { deleteCookie, getCookie, setCookie } from "cookies-next";
 
+const MAX_AGE = 30 * 24 * 60 * 60;
+
 export const cookieStorage: IStorageProvider = {
   async getItem(key: string) {
     const { cookies } = await import("next/headers");
@@ -9,11 +11,11 @@ export const cookieStorage: IStorageProvider = {
     return value ?? null;
   },
   setItem(key: string, value: string) {
-    setCookie(key, value);
+    setCookie(key, value, { maxAge: MAX_AGE });
   },
 
   removeItem(key: string) {
-    deleteCookie(key);
+    deleteCookie(key, { maxAge: 0 });
   },
 };
 
@@ -24,10 +26,10 @@ export const clientCookieStorage: IStorageProvider = {
     return value ?? null;
   },
   setItem(key: string, value: string) {
-    setCookie(key, value);
+    setCookie(key, value, { maxAge: MAX_AGE });
   },
 
   removeItem(key: string) {
-    deleteCookie(key);
+    deleteCookie(key, { maxAge: 0 });
   },
 };
