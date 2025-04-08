68e200e9dd086f019aa4ac6d29c15510142ec2ef
kualta <contact@kualta.dev>
2025-04-08 17:05:05 +0400

    update deleted page

--- src/components/post/post-deleted-view.tsx
+++ src/components/post/post-deleted-view.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { ArticleLayout } from "../navigation/article-layout";
-import { Button } from "../ui/button";
-import Link from "next/link";
+import { DeletedIcon } from "../icons/custom-icons";
 
 const PostDeletedView = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-full p-8">
-      <div className="text-6xl mb-4">(ಥ﹏ಥ)</div>
+      <div className="flex h-64 flex-col items-center justify-center">
+        <DeletedIcon className="mb-4" />
+      </div>
       <h1 className="text-3xl font-bold title mb-4">This post was deleted</h1>
       <p className="text-xl font-medium text-muted-foreground mb-8">
         Sorry, the content you're looking for is no longer available.
