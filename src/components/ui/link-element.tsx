9c6471187d6ab50c14ad73a9a68f69327f7bf4cf
kualta <contact@kualta.dev>
2025-04-08 21:39:55 +0400

    fix links styling

--- src/components/ui/link-element.tsx
+++ src/components/ui/link-element.tsx
@@ -16,7 +16,7 @@ export const LinkElement = withRef<typeof PlateElement>(({ children, className, 
     <PlateElement
       ref={ref}
       as="a"
-      className={cn("text-secondary underline not-prose decoration-secondary underline-offset-4", className)}
+      className={cn("text-foreground underline not-prose decoration-foreground underline-offset-4", className)}
       {...(linkProps as any)}
       {...props}
     >
