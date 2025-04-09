012a89e823c78d46b83317fdfdb9077a80a6fc06
kualta <contact@kualta.dev>
2025-04-09 16:58:52 +0400

    update user page empty state

--- src/components/user/user-name.tsx
+++ src/components/user/user-name.tsx
@@ -3,9 +3,5 @@ import { Account } from "@lens-protocol/client";
 export const UserName = ({ profile, className }: { profile?: Account; className?: string }) => {
   const name = profile?.metadata?.name;
 
-  if (!name) {
-    return null;
-  }
-
-  return <div className={`text-foreground text-3xl font-bold ${className}`}>{name}</div>;
+  return <div className={`text-foreground h-10 text-3xl font-bold ${className}`}>{name}</div>;
 };
