51c643a80e72cc22081f8e8906cad8063808f45d
kualta <contact@kualta.dev>
2025-04-16 12:03:53 +0400

    decouple draft view from post view

--- src/components/draft/draft-view.tsx
+++ src/components/draft/draft-view.tsx
@@ -5,8 +5,10 @@ import { useQueryClient } from "@tanstack/react-query";
 
 import { EvmAddress } from "@lens-protocol/metadata";
 import { toast } from "sonner";
-import { PostView } from "../post/post-view";
 import type { Draft } from "./draft";
+import Markdown from "../misc/markdown";
+import { LazyAuthorView } from "../user/user-author-view";
+import { DraftMenu } from "./draft-menu";
 
 interface DraftViewOptions {
   showDate?: boolean;
@@ -14,6 +16,7 @@ interface DraftViewOptions {
   showTitle?: boolean;
   showSubtitle?: boolean;
   showPreview?: boolean;
+  showContent?: boolean;
 }
 
 interface DraftViewProps {
@@ -43,6 +46,7 @@ export const DraftView = ({
     showSubtitle: true,
     showDate: true,
     showPreview: true,
+    showContent: true,
   },
 }: DraftViewProps) => {
   const { deleteDocument } = useDocumentStorage();
@@ -81,17 +85,116 @@ export const DraftView = ({
     }
   };
 
+  const date = draft.updatedAt ?? draft.createdAt;
+  const title = draft.title ?? undefined;
+  const subtitle = draft.subtitle ?? undefined;
+  const contentMarkdown = draft.contentHtml ?? "";
+  const coverUrl = draft.coverUrl ?? undefined;
+
+  if (options.showContent && !contentMarkdown) {
+    return null;
+  }
+
   return (
-    <PostView
-      item={draft}
-      authors={authors}
-      options={options}
-      isDraft={true}
-      onDelete={handleDelete}
-      isSelected={isSelected}
-      onSelect={onSelect}
-      onEnterSelectionMode={onEnterSelectionMode}
-      isSelectionMode={isSelectionMode}
-    />
+    <div
+      className={`group/draft flex flex-row items-start justify-start gap-4
+      ${isSelected ? "bg-primary/10" : "bg-transparent hover:bg-card/50"}
+      ${isSelectionMode ? "select-none" : ""}
+      hover:text-card-foreground transition-all ease-in duration-100
+      border-0 shadow-none relative w-screen rounded-sm p-4
+      max-w-full sm:max-w-2xl ${options.showPreview ? "h-48" : "h-fit"}`}
+    >
+      {options.showPreview && (
+        <div className="h-40 w-40 shrink-0 aspect-square rounded-sm overflow-hidden">
+          {coverUrl ? (
+            <div className="h-full w-full overflow-hidden">
+              <img
+                src={coverUrl}
+                alt="Cover"
+                className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover/draft:scale-110"
+              />
+            </div>
+          ) : (
+            <div className="h-full w-auto relative">
+              <div className="placeholder-background rounded-sm" />
+            </div>
+          )}
+        </div>
+      )}
+      <div className="flex flex-col w-full h-full justify-between">
+        <div className="flex flex-col w-full gap-2">
+          {options.showAuthor && (
+            <div>
+              <LazyAuthorView authors={authors} />
+            </div>
+          )}
+          {options.showTitle && title && (
+            <div className="text-2xl font-[letter-spacing:var(--title-letter-spacing)] font-[family-name:var(--title-font)] tracking-[-0.8px]  font-semibold font-[color:var(--title-color)] line-clamp-2">
+              {title}
+            </div>
+          )}
+          {options.showSubtitle && subtitle !== "" && (
+            <div className="text-lg font-[family-name:--subtitle-font] text-muted-foreground line-clamp-2">
+              {subtitle}
+            </div>
+          )}
+          {options.showContent && (
+            <div className="whitespace-normal truncate text-sm line-clamp-3 overflow-auto font-[family-name:--paragraph-font] font-[letter-spacing:var(--paragraph-letter-spacing)] font-[family-name:var(--paragraph-font) font-[var(--paragraph-weight)] font-[color:var(--paragraph-color)]">
+              <Markdown
+                content={contentMarkdown}
+                className="prose prose-sm sm:prose-base lg:prose-lg prose-headings:mt-0 prose-headings:mb-0 prose-p:my-0 prose-tight"
+              />
+            </div>
+          )}
+        </div>
+
+        <div className="flex flex-row items-center justify-between text-sm tracking-wide relative z-10">
+          <div className="flex flex-row items-center gap-3">
+            {options.showDate && (
+              <span className="text-muted-foreground">
+                Last updated {date ? new Date(date).toLocaleString() : ""}
+              </span>
+            )}
+          </div>
+          <div className="relative z-10">
+            <DraftMenu
+              draft={draft}
+              onDeleteClick={handleDelete}
+              onSelect={onSelect ?? (() => { })}
+              isSelected={isSelected}
+              onEnterSelectionMode={onEnterSelectionMode}
+              isSelectionMode={isSelectionMode}
+            />
+          </div>
+        </div>
+
+        <div
+          className="absolute inset-0 cursor-pointer z-0"
+          onClick={(e) => {
+            if (isSelectionMode) {
+              onSelect?.();
+            } else if (e.shiftKey) {
+              e.preventDefault();
+              window.getSelection()?.removeAllRanges();
+              onEnterSelectionMode?.();
+            } else {
+              const href = `/write/${draft.documentId}`;
+              window.location.href = href;
+            }
+          }}
+          onPointerDown={(e) => {
+            if (e.pointerType === "touch" && e.isPrimary) {
+              e.preventDefault();
+              window.getSelection()?.removeAllRanges();
+              if (isSelectionMode) {
+                onSelect?.();
+              } else {
+                onEnterSelectionMode?.();
+              }
+            }
+          }}
+        />
+      </div>
+    </div>
   );
 };
--- src/components/post/post-view.tsx
+++ src/components/post/post-view.tsx
@@ -1,12 +1,15 @@
 import { formatDate, formatRelativeTime } from "@/lib/utils";
-import { Post } from "@lens-protocol/client";
+import { Account, Post } from "@lens-protocol/client";
 import { ArticleMetadata, EvmAddress } from "@lens-protocol/metadata";
 import type { Draft } from "../draft/draft";
 import { DraftMenu } from "../draft/draft-menu";
 import Markdown from "../misc/markdown";
-import { LazyAuthorView } from "../user/user-author-view";
+import { AuthorView, LazyAuthorView } from "../user/user-author-view";
 import { PostMenu } from "./post-menu";
 import { PostReactions } from "./post-reactions";
+import Link from "next/link";
+import Image from "next/image";
+import { resolveImageUrl } from "@/lib/utils/resolve-image-url";
 
 interface PostViewOptions {
   showDate?: boolean;
@@ -15,14 +18,13 @@ interface PostViewOptions {
   showSubtitle?: boolean;
   showPreview?: boolean;
   showContent?: boolean;
+  showBlog?: boolean;
 }
 
 interface PostViewProps {
-  item: Post | Draft;
+  post: Post;
   authors: EvmAddress[];
   options?: PostViewOptions;
-  isDraft?: boolean;
-  onDelete?: () => void;
   isSelected?: boolean;
   onSelect?: () => void;
   onEnterSelectionMode?: () => void;
@@ -30,7 +32,7 @@ interface PostViewProps {
 }
 
 export const PostView = ({
-  item,
+  post,
   authors,
   options = {
     showAuthor: true,
@@ -39,48 +41,21 @@ export const PostView = ({
     showDate: true,
     showPreview: true,
     showContent: true,
+    showBlog: true,
   },
   isSelected = false,
   onSelect,
   onEnterSelectionMode,
   isSelectionMode = false,
-  isDraft = false,
-  onDelete,
 }: PostViewProps) => {
-  let contentMarkdown: string;
-  let date: string;
-  let contentJson: string;
-  let username: string;
-  let title: string | undefined;
-  let subtitle: string | undefined;
-  let coverUrl: string | undefined;
-
-  if (isDraft) {
-    const draft = item as Draft;
-    username = "";
-    date = draft.updatedAt ?? draft.createdAt;
-    const content = draft.contentJson ? draft.contentJson : {};
-    title = draft.title ?? undefined;
-    subtitle = draft.subtitle ?? undefined;
-    contentMarkdown = draft.contentHtml ?? "";
-    coverUrl = draft.coverUrl ?? undefined;
-    contentJson = String(draft.contentJson ?? "{}");
-  } else {
-    const post = item as Post;
-    const metadata = post?.metadata as ArticleMetadata;
-
-    date = post.timestamp;
-    title = "title" in metadata ? (metadata.title as string) : undefined;
-    subtitle =
-      (metadata.attributes?.find((attr) => "key" in attr && attr.key === "subtitle")?.value as string) ?? undefined;
-    username = post.author.username?.localName || "";
-    contentMarkdown = "content" in metadata ? (metadata.content as string) : "";
-    coverUrl = metadata.attributes?.find((attr) => "key" in attr && attr.key === "coverUrl")?.value as string;
-    contentJson =
-      (metadata?.attributes?.find((attr) => "key" in attr && attr.key === "contentJson")?.value as string) || "{}";
+  if (!post || post.metadata.__typename !== "ArticleMetadata") {
+    return null;
   }
+  const metadata = post.metadata
+  const blog = post.feed.group?.metadata
+  const blogImage = resolveImageUrl(blog?.icon)
 
-  if (options.showContent && !contentMarkdown) {
+  if (options.showContent && !("content" in metadata && metadata.content)) {
     return null;
   }
 
@@ -95,108 +70,109 @@ export const PostView = ({
     >
       {options.showPreview && (
         <div className="h-40 w-40 shrink-0 aspect-square rounded-sm overflow-hidden">
-          {coverUrl ? (
+          {metadata.attributes?.find((attr) => "key" in attr && attr.key === "coverUrl")?.value ? (
             <div className="h-full w-full overflow-hidden">
               <img
-                src={coverUrl}
+                src={metadata.attributes?.find((attr) => "key" in attr && attr.key === "coverUrl")?.value as string}
                 alt="Cover"
                 className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover/post:scale-110"
               />
             </div>
           ) : (
             <div className="h-full w-auto relative">
-              <div className="placeholder-background rounded-sm" />
+              <div className="placeholder-background rounded-sm">&nbsp;</div>
             </div>
           )}
         </div>
       )}
       <div className="flex flex-col w-full h-full justify-between">
-        <div className="flex flex-col w-full gap-2">
+        <div className="flex flex-row items-center w-full z-10 gap-1 font-[family-name:var(--title-font)] tracking-[-0.8px]">
           {options.showAuthor && (
-            <div>
-              <LazyAuthorView authors={authors} />
-            </div>
+            <AuthorView showUsername={false} accounts={[post.author]} />
           )}
-          {options.showTitle && title && (
-            <div className="text-2xl font-[letter-spacing:var(--title-letter-spacing)] font-[family-name:var(--title-font)] tracking-[-0.8px]  font-semibold font-[color:var(--title-color)] line-clamp-2">
-              {title}
-            </div>
-          )}
-          {options.showSubtitle && subtitle !== "" && (
-            <div className="text-lg font-[family-name:--subtitle-font] text-muted-foreground line-clamp-2">
-              {subtitle}
-            </div>
-          )}
-          {options.showContent && (
-            <div className="whitespace-normal truncate text-sm line-clamp-3 overflow-auto font-[family-name:--paragraph-font] font-[letter-spacing:var(--paragraph-letter-spacing)] font-[family-name:var(--paragraph-font) font-[var(--paragraph-weight)] font-[color:var(--paragraph-color)]">
-              <Markdown
-                content={contentMarkdown}
-                className="prose prose-sm sm:prose-base lg:prose-lg prose-headings:mt-0 prose-headings:mb-0 prose-p:my-0 prose-tight"
-              />
+          {options.showBlog && blog && (
+            <div className="">
+              {options.showAuthor ? (
+                <div className="flex flex-row items-center gap-1">
+                  <span className="text-muted-foreground">in</span>
+                  <Link href={`/b/${post.feed.group?.address}`} className="text-foreground gap-1 flex flex-row items-center hover:underline">
+                    {/* {blogImage && blogImage !== "" && (
+                      <Image src={blogImage} alt={blog.name} width={16} height={16} className="rounded-[4px]" />
+                    )} */}
+                    {blog.name}
+                  </Link>
+                </div>
+              ) : (
+                <span className="text-foreground">{blog.name}</span>
+              )}
             </div>
           )}
         </div>
+        {options.showTitle && metadata.title && (
+          <div className="text-2xl font-[letter-spacing:var(--title-letter-spacing)] font-[family-name:var(--title-font)] tracking-[-0.8px] font-semibold font-[color:var(--title-color)] line-clamp-2">
+            {metadata.title}
+          </div>
+        )}
+        {options.showSubtitle && metadata.attributes?.find((attr) => "key" in attr && attr.key === "subtitle")?.value !== "" && (
+          <div className="text-lg font-[family-name:--subtitle-font] text-muted-foreground line-clamp-2">
+            {metadata.attributes?.find((attr) => "key" in attr && attr.key === "subtitle")?.value as string}
+          </div>
+        )}
+        {options.showContent && (
+          <div className="whitespace-normal truncate text-sm line-clamp-3 overflow-auto font-[family-name:--paragraph-font] font-[letter-spacing:var(--paragraph-letter-spacing)] font-[family-name:var(--paragraph-font) font-[var(--paragraph-weight)] font-[color:var(--paragraph-color)]">
+            <Markdown
+              content={"content" in metadata ? (metadata.content as string) : ""}
+              className="prose prose-sm sm:prose-base lg:prose-lg prose-headings:mt-0 prose-headings:mb-0 prose-p:my-0 prose-tight"
+            />
+          </div>
+        )}
 
         <div className="flex flex-row items-center justify-between text-sm tracking-wide relative z-10">
           <div className="flex flex-row items-center gap-3">
             {options.showDate && (
               <span className="text-muted-foreground">
-                {isDraft ? <>Last updated {formatRelativeTime(date)}</> : formatDate(date)}
+                {formatDate(post.timestamp)}
               </span>
             )}
-            {!isDraft && (
-              <div className="relative z-10">
-                <PostReactions post={item as Post} />
-              </div>
-            )}
+            <div className="relative z-10">
+              <PostReactions post={post} />
+            </div>
           </div>
           <div className="relative z-10">
-            {isDraft ? (
-              <>
-                <DraftMenu
-                  draft={item as Draft}
-                  onDeleteClick={onDelete ?? (() => {})}
-                  onSelect={onSelect ?? (() => {})}
-                  isSelected={isSelected}
-                  onEnterSelectionMode={onEnterSelectionMode}
-                  isSelectionMode={isSelectionMode}
-                />
-              </>
-            ) : (
-              <PostMenu post={item as Post} />
-            )}
+            <PostMenu post={post} />
           </div>
         </div>
+      </div>
 
-        <div
-          className="absolute inset-0 cursor-pointer z-0"
-          onClick={(e) => {
+      <div
+        className="absolute inset-0 cursor-pointer z-0"
+        onClick={(e) => {
+          if (isSelectionMode) {
+            onSelect?.();
+          } else if (e.shiftKey) {
+            e.preventDefault();
+            // Prevent text selection
+            window.getSelection()?.removeAllRanges();
+            onEnterSelectionMode?.();
+          } else {
+            const username = post.author.username?.localName || "";
+            const href = `/p/${username}/${post.slug}`;
+            window.location.href = href;
+          }
+        }}
+        onPointerDown={(e) => {
+          if (e.pointerType === "touch" && e.isPrimary) {
+            e.preventDefault();
+            // Prevent text selection
+            window.getSelection()?.removeAllRanges();
             if (isSelectionMode) {
               onSelect?.();
-            } else if (e.shiftKey) {
-              e.preventDefault();
-              // Prevent text selection
-              window.getSelection()?.removeAllRanges();
-              onEnterSelectionMode?.();
             } else {
-              const href = isDraft ? `/write/${(item as Draft).documentId}` : `/p/${username}/${(item as Post).slug}`;
-              window.location.href = href;
+              onEnterSelectionMode?.();
             }
-          }}
-          onPointerDown={(e) => {
-            if (e.pointerType === "touch" && e.isPrimary) {
-              e.preventDefault();
-              // Prevent text selection
-              window.getSelection()?.removeAllRanges();
-              if (isSelectionMode) {
-                onSelect?.();
-              } else {
-                onEnterSelectionMode?.();
-              }
-            }
-          }}
-        />
-      </div>
+          }
+        }}
+      />
     </div>
   );
 };
