0b71f261f184d998ce7ad1dc53c242f741b93cc6
kualta <contact@kualta.dev>
2025-06-13 20:55:46 +0400

    update landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,27 @@ import Image from "next/image";
 import React from "react";
 import "@/styles/article.css";
 import { useTheme } from "next-themes";
+import { Bird, HighFive, Notes, Sun } from "@/components/icons/landing-images";
 
 const features = [
   {
-    image: "/images/no-tech-1.svg",
+    icon: <Bird />,
     title: "No big tech or vendor lock-in",
     description: (
       <>
-        Fountain is built on user ownership of and decentralisation, secured by{" "}
-        <RoughNotation type="underline" color="hsl(var(--primary))" order={4}>
-          <span className="text-primary whitespace-nowrap">Ethereum</span>
-        </RoughNotation>
+        Fountain is built on user ownership of and decentralisation{" "}
+        {/* <RoughNotation type="underline" color="hsl(var(--primary))" order={4}> */}
+        <span className="text-primary whitespace-nowrap">secured by Ethereum</span>
+        {/* </RoughNotation> */}
         . We believe in{" "}
         <RoughNotation type="underline" color="hsl(var(--primary))" order={4}>
-          <a href="https://github.com/fountain-ink/app" target="_blank" className="text-primary whitespace-nowrap">open-source</a>
+          <a
+            href="https://github.com/fountain-ink/app"
+            target="_blank"
+            className="text-primary whitespace-nowrap"
+          >
+            open-source
+          </a>
         </RoughNotation>{" "}
         and letting users stay in control of their writing and audience.
       </>
@@ -28,18 +35,18 @@ const features = [
     highlightColorDark: "rgba(207, 223, 247, 0.5)",
   },
   {
-    image: "/images/sun-transparent-2.png",
-    title: "No ads - slow social",
+    icon: <Sun />,
+    title: "Slow social",
     description: (
       <>
-        The previous generation of social ran on attention maxxing and squeezing
-        ad revenue out of users. We think users should control and{" "}
-        <RoughNotation type="underline" color="hsl(var(--primary))" order={4}>
-          <span className="text-primary whitespace-nowrap">
+        In the new kind of interenet there's no attention-maxxing and squeezing ad revenue out of users.
+        Fountain is beautifully crafted from the ground up to be <span className="text-primary whitespace-nowrap">fully distractions-free</span>.
+        {/* We believe users should control and{" "} */}
+        {/* <RoughNotation type="underline" color="hsl(var(--primary))" order={4}> */}
+        {/* <span className="text-primary whitespace-nowrap">
             own their content and connections
-          </span>
-        </RoughNotation>
-        .
+          </span> */}
+        {/* </RoughNotation> */}
       </>
     ),
     color: "bg-[#D1EBDB] dark:bg-[#D1EBDB]/70",
@@ -47,22 +54,21 @@ const features = [
     highlightColorDark: "rgba(209, 235, 219, 0.5)",
   },
   {
-    image: "/images/hands-transparent-4.png",
-    title: "Experiments in collaboration",
+    icon: <HighFive />,
+    title: "Collaborate with others",
     description: (
       <>
         Fountain has{" "}
-        <RoughNotation type="underline" color="hsl(var(--primary))" order={4}>
-          <span className="text-primary whitespace-nowrap">
-            multiplayer built in
-          </span>
-        </RoughNotation>
-        , just like Figma or Notion. Beyond that, there are many kinds of groups
-        and zines with super easy{" "}
-        <RoughNotation type="underline" color="hsl(var(--primary))" order={4}>
-          <span className="text-primary whitespace-nowrap">revenue splits</span>
-        </RoughNotation>{" "}
-        out of the box.
+        {/* <RoughNotation type="underline" color="hsl(var(--primary))" order={4}> */}
+        <span className="text-primary whitespace-nowrap">
+          multiplayer built in
+        </span>
+        {/* </RoughNotation> */}
+        . Beyond that, we make it super easy to{" "}
+        {/* <RoughNotation type="underline" color="hsl(var(--primary))" order={4}> */}
+        <span className="text-primary whitespace-nowrap">split revenue</span>
+        {/* </RoughNotation>{" "} */}
+        {" "}with collaborators.
       </>
     ),
     color: "bg-[#FFF4C1] dark:bg-[#FFF4C1]/70",
@@ -70,16 +76,15 @@ const features = [
     highlightColorDark: "rgba(255, 244, 193, 0.5)",
   },
   {
-    image: "/images/notes-svg-4.svg",
-    title: "Become a better writer",
+    icon: <Notes />,
+    title: "Embed anything you want",
     description: (
       <>
-        Fountain is designed to be the world typewriter - no distractions when
-        writing, but{" "}
-        <RoughNotation type="underline" color="hsl(var(--primary))" order={4}>
-          <span className="text-primary whitespace-nowrap">help you grow</span>
-        </RoughNotation>{" "}
-        as a writer through feedback from other users and AI assistants.
+        Our powerful editor is designed to be <span className="text-primary">the world typewriter</span> - with features that{" "}
+        {/* <RoughNotation type="underline" color="hsl(var(--primary))" order={4}> */}
+        <span className="text-primary whitespace-nowrap">help you grow</span>
+        {/* </RoughNotation>{" "} */}
+        {" "}as a writer. Embed music, videos, demos - <span className="text-primary whitespace-nowrap">over 1900+</span> platforms are suported.
       </>
     ),
     color: "bg-[#FFE4E4] dark:bg-[#FFE4E4]/70",
@@ -119,14 +124,13 @@ export default function HomePage() {
               with others
             </RoughNotation>
           </header>
-          <header className="!text-2xl !tracking-tighter !font-normal !mt-8 max-w-2xl mx-auto !text-primary/60 subtitle">
-            Fountain is a blogging platform for experimentation on a new kind of
-            collaborative internet.
+          <header className="!text-2xl !tracking-tighter !font-normal !mt-8 max-w-xl mx-auto !text-primary/60 subtitle">
+            Fountain is a blogging platform for a new kind of internet.
           </header>
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 max-w-6xl mx-auto gap-x-2 gap-y-4 md:gap-y-0 py-8 items-center">
-          {features.map((feature, index) => {
+          {features.map((feature: any, index) => {
             const featureEl = (
               <div
                 className={`flex gap-8 items-start ${index % 2 === 0
@@ -135,15 +139,19 @@ export default function HomePage() {
                   }`}
               >
                 <div
-                  className={`sm:w-40 sm:h-40 w-28 h-28 rounded-lg flex-shrink-0 ${feature.color} overflow-hidden`}
+                  className={`sm:w-40 sm:h-40 w-28 h-28 rounded-lg flex-shrink-0 ${feature.color} overflow-hidden flex items-center justify-center`}
                 >
-                  <Image
-                    src={feature.image}
-                    alt={feature.title}
-                    width={128}
-                    height={128}
-                    className={`w-full h-full object-contain `}
-                  />
+                  {feature.icon ? (
+                    feature.icon
+                  ) : (
+                    <Image
+                      src={feature.image}
+                      alt={feature.title}
+                      width={128}
+                      height={128}
+                      className={`w-full h-full object-contain `}
+                    />
+                  )}
                 </div>
                 <div
                   className={`pt-2 flex-grow ${index % 2 === 0 ? "text-right lg:text-left" : ""
