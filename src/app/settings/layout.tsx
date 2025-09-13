449d0aeae349b40c3e5a367b5cff66daee002fd8
kualta <contact@kualta.dev>
2025-09-13 12:11:40 +0400

    update settings nav

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChartLine, DollarSign, FileSliders, LayoutGrid, Mail, PenTool, Settings, User2, Users } from "lucide-react";
+import { Mail, PenTool, Settings, User2 } from "lucide-react";
 import { TabNavigation } from "@/components/navigation/tab-navigation";
 
 const navItems = [
@@ -8,21 +8,11 @@ const navItems = [
   { id: "blogs", label: "Blogs", icon: PenTool, enabled: true },
   { id: "app", label: "Application", icon: Settings, enabled: true },
   { id: "newsletter", label: "Newsletters", icon: Mail, enabled: true },
-  {
-    id: "subscriptions",
-    label: "Subscriptions",
-    icon: DollarSign,
-    enabled: false,
-  },
-  { id: "editor", label: "Editor", icon: FileSliders, enabled: false },
-  { id: "stats", label: "Stats", icon: ChartLine, enabled: false },
-  { id: "team", label: "Team", icon: Users, enabled: false },
-  { id: "layouts", label: "Layouts", icon: LayoutGrid, enabled: false },
 ];
 
 export default function SettingsLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="container mx-auto p-0 sm:p-6 sm:py-10 max-w-6xl">
+    <div className="container mx-auto p-2 pt-6 sm:p-6 sm:py-10 max-w-6xl">
       <h1 className="text-3xl font-bold ml-4 mb-10">Settings</h1>
       <div className="flex flex-row sm:gap-2 lg:gap-4">
         <TabNavigation navItems={navItems} />
--- src/components/navigation/tab-navigation.tsx
+++ src/components/navigation/tab-navigation.tsx
@@ -34,7 +34,7 @@ export function TabNavigation({ navItems, basePath = "/settings" }: TabNavigatio
           >
             <Link
               href={`${basePath}/${item.id}`}
-              className={`px-2 md:px-4 py-2 flex items-center justify-between rounded-lg text-sm font-medium transition-colors
+              className={`px-2 md:px-4 py-2 w-fit sm:w-full flex items-center justify-between rounded-lg text-sm font-medium transition-colors
                 ${
                   item.enabled
                     ? isActive
